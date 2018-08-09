306d96745dbe7178754101d4f81e73ba2805703f
Hai Liang Wang <hailiang.hl.wang@gmail.com>
2018-08-09 15:19:10 +0800

    增加topic和queue的发送说明

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # Node.js ActiveMQ Get Started
 
 
+## docs
+
 ```
 cd app
 npm install
 cd ../admin
 ./test.sh
 ```
+
+## api
+[http://jmesnil.net/stomp-websocket/doc/](http://jmesnil.net/stomp-websocket/doc/)
+
+
--- app/test/activemq.test.js
+++ app/test/activemq.test.js
@@ -18,12 +18,16 @@ var wait_for = (x) => {
 test("Test activemq # send", async(t) => {
     const client = await activemq.getClient();
     debug("send ...")
+
+    // send 接口默认发送到 queue 中
+    // 比如 下面发送消息 到 queue: /queue/test 下
     client.send("/queue/test", { priority: 9 }, "Hello, STOMP");
 
     var subscription = client.subscribe("/topic/test", (data) => {
         debug('subscribe topic: %j', data);
     });
 
+    // 下面消息会发送到 topic：test 下
     client.send("/topic/test", { priority: 9 }, "Hello, STOMP");
 
     await wait_for(10000)
